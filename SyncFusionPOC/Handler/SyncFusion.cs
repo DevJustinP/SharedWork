3f51fd00112b3d4d578e524a65bb940f2829c783
Justin Pope <pope2014@gmail.com>
2024-02-12 09:12:18 -0600

    Add project files.

---
+++ SyncFusionPOC/Handler/SyncFusion.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAS.Handlers.Handlers
+{
+    public class SyncFusion
+    {
+        public SyncFusion()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2UVhiQlJPdUBEQmFJfFdmR2ldfVRzckUmHVdTRHRbQl9jTH9bdkVmXXdddXI=");
+        }
+
+        public void GeneratePDF(string pHTML, string pFullFilepath)
+        {
+            try
+            {
+                //Initialize HTML to PDF converter.
+                HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+                //Convert URL to PDF document.
+                PdfDocument document = htmlConverter.Convert(pHTML, Path.Combine(AppContext.BaseDirectory, "Docs", "Invoice"));
+                //Create the filestream to save the PDF document. 
+                FileStream fileStream = new FileStream(pFullFilepath, FileMode.CreateNew, FileAccess.ReadWrite);
+                //Save and close the PDF document.
+                document.Save(fileStream);
+                document.Close(true);
+            } catch (Exception ex)
+            {
+                throw new Exception($"{nameof(SyncFusion)}.{nameof(GeneratePDF)} Error - {ex.Message}");
+            }
+        }
+    }
+}
---
+++ SyncFusionPOC/Testing/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using IAS.Handlers.Handlers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        SyncFusion _SyncFusion = new SyncFusion();
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            if (!Directory.Exists(Path.Combine(AppContext.BaseDirectory, nameof(UnitTest1)))) 
+            {
+                Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, nameof(UnitTest1)));
+            }
+            if (!Directory.Exists(Path.Combine(AppContext.BaseDirectory, nameof(UnitTest1), nameof(PDF_Test))))
+            {
+                Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, nameof(UnitTest1), nameof(PDF_Test)));
+            }
+            else
+            {
+                foreach (var file in Directory.GetFiles(Path.Combine(AppContext.BaseDirectory, nameof(UnitTest1), nameof(PDF_Test))))
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void PDF_Test()
+        {
+            try
+            {
+                _SyncFusion.GeneratePDF("<h1>Hello World</h1>", Path.Combine(AppContext.BaseDirectory, nameof(UnitTest1), nameof(PDF_Test), "PDF_Test.pdf"));
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+            Assert.IsTrue(File.Exists(Path.Combine(AppContext.BaseDirectory, nameof(UnitTest1), nameof(PDF_Test), "PDF_Test.pdf")));
+        }
+    }
+}
